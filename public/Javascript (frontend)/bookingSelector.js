abbd144787024eafbf588e9739509cd94d75c946
Renz <renzonifico@gmail.com>
2025-03-09 23:44:18 +0800

    changes currently displayed rooms based on current hall selected

---
+++ public/Javascript (frontend)/bookingSelector.js
@@ -0,0 +1,24 @@
+const halls = document.querySelector("#building")
+const rooms = document.querySelector("#room")
+
+const roomDirectory = {
+    goks: ["GK101A", "GK304A", "GK304B"],
+    andrew: ["AG702", "AG1904"],
+    st_la_salle: ["LS209", "LS311"]
+}
+
+//change displayed rooms based on current hall
+halls.addEventListener("change", (e) => { 
+    e.preventDefault()
+    
+    const currentHall = halls.value
+
+    rooms.innerHTML = ""
+    for(let currentRoom of roomDirectory[currentHall]){
+        let roomElement = document.createElement("option")
+        roomElement.setAttribute("name", currentRoom)
+        roomElement.setAttribute("value", currentRoom)
+        roomElement.innerText = currentRoom
+        rooms.append(roomElement)
+    }
+})
